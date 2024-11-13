7dbeb81e5276ae7935515bf2779842152cf04b08
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-13 20:26:35 -0300

    create method main

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello world")
+import flet as ft
+from todo_app import TodoApp
+
+
+def main(page: ft.Page):
+    page.title = "ToDo App"
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+    page.scroll = ft.ScrollMode.ADAPTIVE
+
+    page.add(TodoApp())
+
+
+ft.app(target=main)
